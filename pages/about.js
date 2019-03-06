ef493077d2b91e68f28194ad8e6df0f822605fcc
Tucker <trblackw@ncsu.edu>
2019-03-05 22:53:33 -0500

    imported Link from next

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 const About = () => {
   return (
      <div>
         <h1>About</h1>
+        <Link href="/">
+        <h1>Home</h1>
+        </Link>
         <img src="../static/React.js_logo.png" alt=""/>
       
     </div>
