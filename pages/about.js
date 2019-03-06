63388db9b3d3f5db7845c8c0ad7d301f67dbcdef
Tucker <trblackw@ncsu.edu>
2019-03-05 22:44:12 -0500

    added static directory for images

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+     <div>
+        <h1>About</h1>
+        <img src="../static/React.js_logo.png" alt=""/>
+      
+    </div>
+  )
+}
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      Welcome to the homepage
+    </div>
+  )
+}
+
+export default index
