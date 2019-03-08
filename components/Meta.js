bc4c81bdebe7470379570fe07eed852be6ecbe08
Tucker <trblackw@ncsu.edu>
2019-03-08 17:04:40 -0500

    passing styled-components config to Meta component

--- components/Meta.js
+++ components/Meta.js
@@ -1,11 +1,13 @@
 import Head from 'next/head';
 
-const Meta = () => (
+const Meta = ({ styleTags }) => (
 	<Head>
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<meta charSet="utf-8" />
+      <meta charSet="utf-8" />
+      <meta name="robots" content="noindex, nofollow" />
 		<link rel="shortcut icon" href="/static/favicon.png" />
-		<link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+      <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+      {styleTags}
 		<title>My awesome site</title>
 	</Head>
 );
