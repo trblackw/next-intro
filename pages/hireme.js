94be0ca3ddaf130775def537e82ca4c84e530be6
Tucker <trblackw@ncsu.edu>
2019-03-11 17:53:15 -0400

    added placeholder for hireme page

---
+++ pages/hireme.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout';
+
+export default () => (
+	<Layout title="Hire me">
+		<p>
+			You can connect with me @ <a href="mailto:tuckerblackwell.dev@gmail.com">tuckerblackwell.dev@gmail.com</a>
+		</p>
+	</Layout>
+);
