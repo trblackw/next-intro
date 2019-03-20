842e3cd133ce764d6edf7e7ea4e16208359e2058
Tucker <trblackw@ncsu.edu>
2019-03-20 18:20:08 -0400

    set up getInitialProps function

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Layout from '../components/Layout';
 
-export default () => (
-	<Layout title="About">
-		<Link href="/">
-			<h1>Home</h1>
-		</Link>
-		<img src="../static/React.js_logo.png" alt="" />
-	</Layout>
-);
+const About = ({ username }) => {
+	const [user, setUser] = useState(username);
+
+   About.getInitialProps = async () => {
+      const res = await fetch('https://api.github.com/users/trblackw');
+      const { login: username } = await res.json();
+      return { username }
+	};
+	return (
+		<Layout title="About">
+			{user}
+			<Link href="/">
+				<h1>{username}</h1>
+			</Link>
+			<img src="../static/React.js_logo.png" alt="" />
+		</Layout>
+	);
+};
+
+export default About;
