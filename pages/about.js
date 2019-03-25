3653c7c5efa5f45c54daaab51c5eb7ce0c233db9
Tucker <trblackw@ncsu.edu>
2019-03-25 19:33:00 -0400

    modify component to class component for static genInitialProps method

--- pages/about.js
+++ pages/about.js
@@ -1,24 +1,24 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Component } from 'react';
+import fetch from 'isomorphic-unfetch';
 import Link from 'next/link';
 import Layout from '../components/Layout';
 
-const About = ({ username }) => {
-	const [user, setUser] = useState(username);
+export default class About extends Component {
+	static async getInitialProps() {
+		const res = await fetch('https://api.github.com/users/trblackw');
+      const data = await res.json();
+      
+      return { user: data }
+	}
 
-   About.getInitialProps = async () => {
-      const res = await fetch('https://api.github.com/users/trblackw');
-      const { login: username } = await res.json();
-      return { username }
-	};
-	return (
-		<Layout title="About">
-			{user}
-			<Link href="/">
-				<h1>{username}</h1>
-			</Link>
-			<img src="../static/React.js_logo.png" alt="" />
-		</Layout>
-	);
-};
-
-export default About;
+	render() {
+		return (
+			<Layout title="About">
+				<Link href="/">
+					<h1 style={{ color: 'black' }}>{JSON.stringify(this.props.user)}</h1>
+				</Link>
+				<img src="../static/React.js_logo.png" alt="" />
+			</Layout>
+		);
+	}
+}
